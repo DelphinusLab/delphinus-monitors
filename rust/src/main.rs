2613a4dd75f71440944623ea7b4dea21fa3e0248
akayi07 <zhuolimiaotx@163.com>
2021-10-22 12:41:19 +0800

    connect to the database

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,6 +3,9 @@ use ethcontract::futures::TryStreamExt;
 use url::Url;
 use structopt::StructOpt;
 use std::process;
+use std::fs::File;
+use std::io::BufReader;
+use serde_json::Value;
 
 const MONITOR_ACCOUNT:&str = "0x6f6ef6dfe681b6593ddf27da3bfde22083aef88b";
 const BSC_PROVIDER:&str = "wss://bsc.getblock.io/testnet/?api_key=182a8e0d-c03a-44ac-b856-41d2e47801db";
@@ -55,7 +58,7 @@ async fn main() -> Result<(),  web3::Error> {
                 bri_info
             },
             Err(e) => {
-                println!("Get the bridge ingo error: {}.", e);
+                println!("Get the bridge info failed: {}.", e);
                 process::exit(1);
             }
         };
@@ -80,5 +83,67 @@ async fn main() -> Result<(),  web3::Error> {
     for e in &event_history_vec {
         println!("Event: {:?}", e);
     };
+
+    let path = "./src/config.json";
+    let file = match File::open(path) {
+        Ok(file) => file,
+        Err(e) => {
+            println!("open failed: {}", e);
+            process::exit(1);
+        }
+    };
+    // Open the file in read-only mode with buffer.
+    let reader = BufReader::new(file);
+    // Read the JSON contents of the file as an instance.
+    let config: Value = match serde_json::from_reader(reader) {
+        Ok(c) => c,
+        Err(e) => {
+            println!("Read file error: {}", e);
+            process::exit(1);
+        }
+    };
+    let address = bridge_instance.address().to_string();
+    let mongodb_url = config["localtestnet1"]["mongodb_url"].as_str().unwrap();
+    let network_id = config["localtestnet1"]["device_id"].as_str().unwrap();
+    let url = network_id.to_owned() + &address;
+    println!("The name of database is: {}", url);
+    let client_options = match ClientOptions::parse(&mongodb_url).await {
+        Ok(c) => c,
+        Err(e) => {
+            println!("Parse the MongoDB connection string failed: {}", e);
+            process::exit(1);
+        }
+    };
+
+    let client = match Client::with_options(client_options)
+    {
+        Ok(c) => c,
+        Err(e) => {
+            println!("Create the MongoDB client failed: {}", e);
+            process::exit(1);
+        }
+    };
+
+    match client.database(&url).run_command(doc! {"ping": 1}, None).await {
+        Ok(_) => (),
+        Err(e) => {
+            println!("Create the MongoDB client failed: {}", e);
+            process::exit(1);
+        }
+    }
+
+    println!("Connected successfully.");
+    match client.list_database_names(None, None).await {
+        Ok(dbs) => {
+            println!("All database:");
+            for dbname in dbs {
+                println!("{}", dbname);
+            }
+        },
+        Err(e) => {
+            println!("Create the MongoDB client failed: {}", e);
+            process::exit(1);
+        }
+    }
     Ok(())
 }
