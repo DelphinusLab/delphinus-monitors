995607d55e4387b2c98de286f77a67955eab477e
Zhang Junyu <junyu92@gmail.com>
2021-11-11 18:03:32 +0800

    refactor l2 monitor

--- src/substrate/index.ts
+++ src/substrate/index.ts
@@ -1,23 +1,37 @@
 import BN from "bn.js";
 import substrateNode from "../../config/substrate-node.json";
-import { SubstrateClient } from "./client";
+import { SubstrateClient, withL2Client } from "./client";
 import { L2Ops } from "./enums";
 import { CommandOp, L2Storage } from "delphinus-zkp/src/zokrates/command";
 import { runZkp } from "delphinus-zkp/src/zokrates/main";
 import { Field } from "delphinus-curves/src/field";
-import { bridgeInfos, registerBridge } from "./bridges";
+import { withBridgeClient, Bridge } from "solidity/clients/bridge/bridge";
 import { EthConfig } from "solidity/clients/config";
-import { Bridge } from "solidity/clients/bridge/bridge";
 
 const MonitorETHConfig: any = require("../../config/eth-config.json");
-
 /* We should using local secrets instead of debuggin secrets */
 const Secrets: any = require("solidity/.secrets.json");
 
 const SECTION_NAME = "swapModule";
 
-async function loadL2Storage() {
-  return new L2Storage();
+async function withL2Storage<t>(cb: (_: L2Storage) => Promise<t>) {
+  let l2Storage = new L2Storage();
+
+  try {
+    return await cb(l2Storage);
+  } finally {
+    await l2Storage.closeDb();
+  }
+}
+
+async function foreachL1Bridge<t>(
+  configs: any[],
+  cb: (bridge: Bridge) => Promise<t>
+) {
+  configs.forEach(
+    async (config: any) =>
+      await withBridgeClient(EthConfig[config.chainName](Secrets), false, cb)
+  );
 }
 
 export function dataToBN(data: any) {
@@ -37,14 +51,14 @@ const opsMap = new Map<L2Ops, CommandOp>([
 ]);
 
 async function verify(
-  bridge: any,
+  bridge: Bridge,
   l2Account: string,
   command: BN[],
   proof: BN[],
   rid: BN,
   vid: number = 0
 ) {
-  console.log("start to send to:", bridge.chain_hex_id);
+  console.log("start to send to:", bridge.getChainIdHex());
   while (true) {
     let txhash = "";
     try {
@@ -63,7 +77,7 @@ async function verify(
        * 2. check if the rid exists in the db
        * 3. get hash from db, and check if it is pending in eth
        */
-      let tx = bridge.verify(0, command, proof, vid, 0, rid);
+      let tx = bridge.verify("0", command, proof, vid, 0, rid);
       let r = await tx.when("Verify", "transactionHash", (hash: string) => {
         console.log("Get transactionHash", hash);
         txhash = hash;
@@ -79,7 +93,7 @@ async function verify(
         if (e.message == "ESOCKETTIMEDOUT") {
           await new Promise((resolve) => setTimeout(resolve, 5000));
         } else if (e.message == "nonce too low") {
-          console.log("failed on:", bridge.chain_hex_id, e.message); // not sure
+          console.log("failed on:", bridge.getChainIdHex(), e.message); // not sure
           return;
         } else {
           console.log("Unhandled exception during verify");
@@ -123,15 +137,12 @@ async function handleOp(
   console.log(proofBuffer);
   console.log("----- verify args -----");
 
-  for (const bridgeInfo of bridgeInfos) {
-    await verify(
-      bridgeInfo.bridge,
-      "0",
-      commandBuffer,
-      proofBuffer,
-      new BN(rid, 10)
-    );
-  }
+  foreachL1Bridge(
+    MonitorETHConfig.filter((config: any) => config.enable),
+    async (bridge: Bridge) => {
+      await verify(bridge, "", commandBuffer, proofBuffer, new BN(rid, 10));
+    }
+  );
 
   console.log("before end snapshot", rid);
   await storage.endSnapshot();
@@ -162,61 +173,41 @@ async function handlePendingReq(kv: any[], storage: L2Storage) {
   }
 }
 
-async function handlePendingReqs(client: SubstrateClient, storage: L2Storage) {
+async function getPendingReqs(client: SubstrateClient) {
   /* transactions that havn't receive ack. */
   const txMap = await client.getPendingReqMap();
-  const txList = Array.from(txMap.entries())
+  return Array.from(txMap.entries())
     .map((kv: any) => [dataToBN(kv[0]), kv[1]])
     .sort((kv1: any, kv2: any) => kv1[0] - kv2[0]);
+}
+
+async function main() {
+  let txList = await withL2Client(
+    `${substrateNode["host"]}:${substrateNode.port}`,
+    async (l2Client: SubstrateClient) => {
+      return await getPendingReqs(l2Client);
+    },
+    undefined
+  );
+
   console.log("pending req length", txList.length);
+  if (txList.length === 0) {
+    console.log("no pending req, exiting...");
+    return;
+  }
 
-  if (txList.length !== 0) {
-    const commitedRid = new BN(txList[0][0].toString(), 10).subn(1);
+  const commitedRid = new BN(txList[0][0].toString(), 10).subn(1);
 
+  withL2Storage(async (storage: L2Storage) => {
     console.log("checkout db snapshot to", commitedRid.toString(10));
     await storage.loadSnapshot(commitedRid.toString(10));
 
     for (const kv of txList) {
       await handlePendingReq(kv, storage);
     }
+  });
 
-    console.log("resolve ", txList.length, " pending reqs, exiting...");
-  } else {
-    console.log("no pending req, exiting...");
-  }
-}
-
-async function main() {
-  const client = new SubstrateClient(
-    `${substrateNode["host"]}:${substrateNode.port}`
-  );
-
-  const storage = await loadL2Storage();
-
-  for (let config of MonitorETHConfig.filter((config: any) => config.enable)) {
-    const bridge = await new Bridge(
-      EthConfig[config.chainName](Secrets),
-      false
-    );
-    await bridge.init();
-    registerBridge(config.chainName, bridge);
-  }
-
-  console.log("getBridge");
-
-  await client.init();
-
-  try {
-    await handlePendingReqs(client, storage);
-  } catch (e) {
-    console.log("catch an exception, ", e);
-  }
-
-  for (const bridge of bridgeInfos) {
-    await bridge.bridge.close();
-  }
-  await storage.closeDb();
-  await client.close();
+  console.log("resolve ", txList.length, " pending reqs, exiting...");
 }
 
 main();
