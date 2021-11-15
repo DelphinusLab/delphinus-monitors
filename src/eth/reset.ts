ac00e571fd6942d1864459158375afcdf9db3afc
Zhang Junyu <junyu92@gmail.com>
2021-11-15 18:43:41 +0800

    rewrite all files with typescript

---
+++ src/eth/reset.ts
@@ -0,0 +1,23 @@
+import { EventTracker } from "web3subscriber/src/sync-pending-events";
+import { EthConfigEnabled } from "delphinus-deployment/src/config";
+
+const BridgeJSON = require("solidity/build/contracts/Bridge.json");
+
+async function main() {
+  const config = EthConfigEnabled.find(config => config.chain_name === process.argv[2])!;
+  console.log("config:", config);
+
+  let etracker = new EventTracker(
+    config.device_id,
+    BridgeJSON,
+    config.ws_source,
+    config.monitor_account,
+    config.mongodb_url,
+    async (n, v, h) => {}
+  );
+
+  await etracker.resetEvents();
+  console.log("reset events done.");
+}
+
+main();
--- src/tools/markle-tree.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const command_1 = require("delphinus-zkp/src/business/command");
-console.log(new command_1.L2Storage().root);
-//# sourceMappingURL=markle-tree.js.map
