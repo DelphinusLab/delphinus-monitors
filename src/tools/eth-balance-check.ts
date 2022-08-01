5d0f6273eb21a70afabfa4614c554c2ed9bc65da
RuilaiZhang <ray.zhangruilai@gmail.com>
2022-08-01 17:20:25 +1000

    update

--- src/tools/eth-balance-check.ts
+++ src/tools/eth-balance-check.ts
@@ -1,7 +1,6 @@
 import Web3 from "web3";
 
 export async function checkEthBalance(provider: string, address: string) {
-  console.log("checking eth balance")
   const web3 = new Web3(new Web3.providers.HttpProvider(provider));
   await web3.eth.getBalance(address, function(err, result) {  
     if (err) {
