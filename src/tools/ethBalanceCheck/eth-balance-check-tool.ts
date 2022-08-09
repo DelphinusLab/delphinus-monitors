0712382cda134860cc7cebe5e0f6bb8931c43d30
RuilaiZhang <ray.zhangruilai@gmail.com>
2022-08-09 13:46:48 +1000

    balace-check-tool enhancement

--- src/tools/ethBalanceCheck/eth-balance-check-tool.ts
+++ src/tools/ethBalanceCheck/eth-balance-check-tool.ts
@@ -1,19 +1,21 @@
 import { getConfigByChainName } from "delphinus-deployment/src/config";
 import { L1ClientRole } from "delphinus-deployment/src/types";
-import { checkDeploymentAccountEthBalance } from "./eth-balance-check"
+import { checkDeployerAccountBalance } from "./eth-balance-check"
 
 async function main(chainName: string, warningAmount: string) {
     console.log("start calling");
     const defaultWarningAmount = "1";
-    let checkInfo;
+    let checkInfo, Warning;
     let config = await getConfigByChainName(L1ClientRole.Monitor, chainName);
     if(warningAmount == undefined){
-      checkInfo =  await checkDeploymentAccountEthBalance(config, defaultWarningAmount);
+      Warning =  defaultWarningAmount;
     }else{
-      checkInfo =  await checkDeploymentAccountEthBalance(config, warningAmount);
+      Warning =  warningAmount;
     }
+    checkInfo =  await checkDeployerAccountBalance(config, Warning);
     if (!checkInfo[0]){
-      console.log("Your ETH Balance is: " + checkInfo[1] + " ETH");
+      console.log("Congrats: Deployer's balance is More than WarningAmount(" + Warning + checkInfo[2] + ")");
+      console.log("Deployer's balance is: " + checkInfo[1] + checkInfo[2]);
     }
   }
   
--- src/tools/ethBalanceCheck/eth-balance-check.ts
+++ src/tools/ethBalanceCheck/eth-balance-check.ts
@@ -1,31 +1,35 @@
 import Web3 from "web3";
 import { ChainConfig } from "delphinus-deployment/src/types";
 
-export async function checkDeploymentAccountEthBalance(config: ChainConfig, warningAmount: string){
+export async function checkDeployerAccountBalance(config: ChainConfig, warningAmount: string){
   if (config.privateKey === "") {
-    return checkEthBalance(config.wsSource, config.monitorAccount, warningAmount);
+    return checkEthBalance(config.wsSource, config.privateKey, config.deviceId, warningAmount);
   } else {
-    return checkEthBalance(config.rpcSource, config.monitorAccount, warningAmount);
+    return checkEthBalance(config.rpcSource, config.privateKey, config.deviceId, warningAmount);
   }
 }
 
-export async function checkEthBalance(providerSource: string, address: string, warningAmount: string) {
+export async function checkEthBalance(providerSource: string, privateKey: string, ChainId: string, warningAmount: string) {
   let ethBalanceWarning = false;
   let balance;
   const web3 = getWeb3FromSource(providerSource);
+  console.log("Deployer's privateKey: " + privateKey);
+  const address = web3.eth.accounts.privateKeyToAccount(privateKey).address;
+  const currentSymbol = getNativeCurrencySymbolByChainId(ChainId);
+  console.log("Deployer's address: " + address);
   await web3.eth.getBalance(address, function(err, result) {  
     if (err) {
       console.log(err)
     } else {
       balance = Web3.utils.fromWei(result, "ether");
-      if (parseInt(result, 10) < parseInt(Web3.utils.toWei(warningAmount, "ether"), 10)){
+      if (parseInt(balance, 10) < parseInt(warningAmount, 10)){
         ethBalanceWarning = true;
-        console.log("Warning: Your ETH Balance is less than " + warningAmount + " ETH");
-        console.log("Your current ETH balance: " + balance + " ETH");
+        console.log("Warning: Deployer's balance is less than " + warningAmount + currentSymbol);
+        console.log("Current balance: " + balance + currentSymbol);
       }
     }
   });
-  return [ethBalanceWarning, balance]
+  return [ethBalanceWarning, balance, currentSymbol]
 }
 
 function getWeb3FromSource(provider: string) {
@@ -34,5 +38,17 @@ function getWeb3FromSource(provider: string) {
     return new Web3(new Web3.providers.HttpProvider(provider));
   }else {
     return new Web3(new Web3.providers.WebsocketProvider(provider));
+  }
+}
+
+function getNativeCurrencySymbolByChainId(chainId: string){
+  if(chainId == "97"){
+    return " tBNB";
+  }else if(chainId == "3"){
+    return " ROP";
+  }else if (chainId == "338"){
+    return " TCRO";
+  }else{
+    return " Unkown Token"
   }
 }
