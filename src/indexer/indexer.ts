565be48fc1630bfb7e31f8207c8710a558cdd9de
Richard Hao <richard@rhao.io>
2022-11-15 09:12:56 +0000

    store last block checked in local file

--- src/indexer/indexer.ts
+++ src/indexer/indexer.ts
@@ -1,23 +1,29 @@
-import BN from "bn.js";
 import { dataToBN, SubstrateClient, withL2Client } from "../substrate/client";
-
+import fs from "fs";
 import { eventRecorder, latestDbTx, clearDb } from "./indexStorage";
 import { L1ClientRole } from "delphinus-deployment/src/types";
 import { getEnabledEthConfigs } from "delphinus-deployment/src/config";
-import { CommandOp } from "delphinus-l2-client-helper/src/swap";
-import { ExtrinsicSuccess, ExtrinsicFail } from "./types";
 
 async function main() {
   //sync up to latest blocks, and then start listening for new events
   //get latest block stored in DB
+
+  //clear the database,
   if (process.argv[2] === "new") {
     await clearDb();
+    fs.unlink("./lastBlock.txt", () => {});
   }
-  //await clearDb(); //Use this to drop the collection (for testing)
-  //issue is if there are no transactions for while, it will resync from latest tx not latest block
-  //so perhaps store latest block or just record all blocks regardless of TX
+
   let lastEntry = await latestDbTx();
-  console.log("latestBlock in DB:", lastEntry);
+  console.log("latestTransaction in DB:", lastEntry);
+  //read a file with the latest block number otherwise create a new file
+  let lastBlock = 1;
+  if (fs.existsSync("./latestBlock.txt")) {
+    lastBlock = parseInt(fs.readFileSync("./latestBlock.txt").toString());
+  } else {
+    fs.writeFileSync("./latestBlock.txt", "1");
+  }
+  console.log("latestBlock in file:", lastBlock);
 
   try {
     console.log("starting l2 indexer");
@@ -27,11 +33,12 @@ async function main() {
       )[0].l2Account,
       async (l2Client: SubstrateClient) => {
         let latestBlock = await l2Client.lastHeader;
-        let blockToSync = lastEntry!.length > 0 ? lastEntry![0].blockNumber : 1;
+        let blockToSync = lastBlock;
         for (let i = blockToSync; i <= latestBlock.number.toNumber(); i++) {
           let txs = await l2Client.syncBlockExtrinsics(i);
           console.log("tx:", txs);
           if (txs.length > 0) await eventRecorder(txs); //batch record for each block, insert all txs from a block at once
+          fs.writeFileSync("./latestBlock.txt", i.toString());
         }
       }
     );
@@ -43,4 +50,7 @@ async function main() {
   process.exit();
 }
 
-main();
+main().catch((e) => {
+  console.log(e);
+  process.exit();
+});
