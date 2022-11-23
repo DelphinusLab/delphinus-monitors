03e080a44c9d722f891645f07b360ba8be889971
yymone <yymone84@hotmail.com>
2022-11-23 14:44:59 +1100

    force monitor to sleep 5 seconds for each run

--- run_l1monitor.sh
+++ run_l1monitor.sh
@@ -3,5 +3,5 @@ time=$(date +%Y-%m-%d-%H-%M-%S)
 while true
 do
   node --unhandled-rejections=strict src/eth/monitor.js $1 $2 | tee -a l1MonitorLogs/l1MonitorLogs_$1_$time
-  sleep 4
+  sleep 5
 done
