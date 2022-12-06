faa0fae3f0d8324fe16e58ca1d7829d585b2259b
root <root@Ray-Desktop.localdomain>
2022-12-06 22:44:37 +1100

    change logic and add unit test

--- src/tools/getBufferBlocks.ts
+++ src/tools/getBufferBlocks.ts
@@ -1,10 +1,8 @@
-import { getConfigByChainName } from "delphinus-deployment/src/config";
-import { L1ClientRole } from "delphinus-deployment/src/types";
+import { ChainConfig } from "delphinus-deployment/src/types";
 
-export async function getBufferBlocks(chainName:string) {
-    const config = await getConfigByChainName(L1ClientRole.Monitor, chainName);
+export async function getBufferBlocks(config: ChainConfig) {
     let bufferBlocks = 20;
-    if(config.bufferBlocks && !isNaN(Number(config.bufferBlocks))){
+    if(config.bufferBlocks != undefined && !isNaN(Number(config.bufferBlocks))){
         if(config.bufferBlocks >= 0){
             bufferBlocks = config.bufferBlocks;
         }
--- tests/getBufferBlock.test.ts
+++ tests/getBufferBlock.test.ts
@@ -1,14 +1,86 @@
 import { getBufferBlocks } from "../src/tools/getBufferBlocks";
 
 describe("test getBufferBlocks works", () => {
-    test("getBufferBlocks works case 1", async () => {
+    test("getBufferBlocks works case 1 with bufferBlocks", async () => {
         jest.setTimeout(60000); //1 minute timeout
-        let bufferBlocks = await getBufferBlocks("goerli");
+        let config = {
+            chainName: "",
+            mongodbUrl: "",
+            syncEventsStep: 0,
+            bufferBlocks: 20,
+            gasWarningAmount: "",
+            rpcSource:"",
+            wsSource:"",
+            privateKey: "",
+            monitorAccount: "",
+            deviceId: "",
+            l2Account: "",
+            enabled: true,
+            isSnap: false,
+          }
+        let bufferBlocks = await getBufferBlocks(config);
         expect(bufferBlocks).toEqual(20);
     });
-    test("getBufferBlocks works case 2", async () => {
+
+    test("getBufferBlocks works case 2 without bufferBlocks", async () => {
         jest.setTimeout(60000); //1 minute timeout
-        let bufferBlocks = await getBufferBlocks("rolluxtestnet");
+        let config = {
+            chainName: "",
+            mongodbUrl: "",
+            syncEventsStep: 0,
+            gasWarningAmount: "",
+            rpcSource:"",
+            wsSource:"",
+            privateKey: "",
+            monitorAccount: "",
+            deviceId: "",
+            l2Account: "",
+            enabled: true,
+            isSnap: false,
+          }
+        let bufferBlocks = await getBufferBlocks(config);
         expect(bufferBlocks).toEqual(20);
+    });
+
+    test("getBufferBlocks works case 3 with negative bufferBlocks", async () => {
+        jest.setTimeout(60000); //1 minute timeout
+        let config = {
+            chainName: "",
+            mongodbUrl: "",
+            syncEventsStep: 0,
+            bufferBlocks: -2,
+            gasWarningAmount: "",
+            rpcSource:"",
+            wsSource:"",
+            privateKey: "",
+            monitorAccount: "",
+            deviceId: "",
+            l2Account: "",
+            enabled: true,
+            isSnap: false,
+          }
+        let bufferBlocks = await getBufferBlocks(config);
+        expect(bufferBlocks).toEqual(20);
+    });
+
+    test("getBufferBlocks works case 4 with 0 bufferBlocks", async () => {
+        jest.setTimeout(60000); //1 minute timeout
+        let config = {
+            chainName: "",
+            mongodbUrl: "",
+            syncEventsStep: 0,
+            bufferBlocks: 0,
+            gasWarningAmount: "",
+            rpcSource:"",
+            wsSource:"",
+            privateKey: "",
+            monitorAccount: "",
+            deviceId: "",
+            l2Account: "",
+            enabled: true,
+            isSnap: false,
+          }
+        let bufferBlocks = await getBufferBlocks(config);
+        expect(bufferBlocks).toEqual(0);
     });
 })
