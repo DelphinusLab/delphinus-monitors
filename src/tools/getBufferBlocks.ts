061ec4c0466a8f639f7e88ef808744cd133d04ab
RuilaiZhang <ray.zhangruilai@gmail.com>
2022-12-07 15:35:51 +1100

    change getBufferBlocks

--- src/tools/getBufferBlocks.ts
+++ src/tools/getBufferBlocks.ts
@@ -1,9 +1,14 @@
 export async function getBufferBlocks(buffer?:number) {
-    let bufferBlocks = 20;
+    let bufferBlocks: number;
+    const defaltBufferBlocks = 0;
     if(buffer != null && !isNaN(Number(buffer))){
         if(buffer >= 0){
             bufferBlocks = buffer;
+        }else{
+            bufferBlocks = defaltBufferBlocks;
         }
+    }else {
+        bufferBlocks = defaltBufferBlocks;
     }
     return bufferBlocks
 }
--- tests/getBufferBlock.test.ts
+++ tests/getBufferBlock.test.ts
@@ -12,14 +12,14 @@ describe("test getBufferBlocks works", () => {
         jest.setTimeout(60000); //1 minute timeout
         let buffer: undefined;
         let bufferBlocks = await getBufferBlocks(buffer);
-        expect(bufferBlocks).toEqual(20);
+        expect(bufferBlocks).toEqual(0);
     });
 
     test("getBufferBlocks works case 3 with negative bufferBlocks", async () => {
         jest.setTimeout(60000); //1 minute timeout
         let buffer = -2;
         let bufferBlocks = await getBufferBlocks(buffer);
-        expect(bufferBlocks).toEqual(20);
+        expect(bufferBlocks).toEqual(0);
     });
 
     test("getBufferBlocks works case 4 with 0 bufferBlocks", async () => {
